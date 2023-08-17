f2d81e7173bed40097eecf70486fc386540352d5
shaibautista1 <142084301+shaibautista1@users.noreply.github.com>
2023-08-17 10:35:15 +0800

    Add files via upload

---
+++ assets/dist/js/script.js
@@ -0,0 +1,18 @@
+document.querySelector('.link-1').addEventListener('click', function() {
+    if (window.innerWidth <= 768) document.querySelector('.navbar-collapse').classList.toggle('show');
+});
+document.querySelector('.link-2').addEventListener('click', function() {
+    if (window.innerWidth <= 768) document.querySelector('.navbar-collapse').classList.toggle('show');
+});
+document.querySelector('.link-3').addEventListener('click', function() {
+    if (window.innerWidth <= 768) document.querySelector('.navbar-collapse').classList.toggle('show');
+});
+document.querySelector('.link-4').addEventListener('click', function() {
+    if (window.innerWidth <= 768) document.querySelector('.navbar-collapse').classList.toggle('show');
+});
+document.querySelector('.link-5').addEventListener('click', function() {
+    if (window.innerWidth <= 768) document.querySelector('.navbar-collapse').classList.toggle('show');
+});
+document.querySelector('.link-6').addEventListener('click', function() {
+    if (window.innerWidth <= 768) document.querySelector('.navbar-collapse').classList.toggle('show');
+});
